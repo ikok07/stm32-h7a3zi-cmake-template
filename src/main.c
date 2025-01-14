20dff50ae5e6f0d9c31b4c309a3f94bb955f12fc
Kaloyan Stefanov <kokmarok@gmail.com>
2025-01-14 11:16:43 +0200

    add files

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "stm32h723xx.h"
+
+#define GPIOBEN		(1U<<1)
+#define PIN14		(1U<<14)
+#define LED_PIN		PIN14
+
+int main(void)
+{
+    RCC->AHB4ENR |= GPIOBEN;
+
+    GPIOB->MODER |= (1U<<28);
+    GPIOB->MODER &=~ (1U<<29);
+
+    while (1) {
+        GPIOB->ODR ^= LED_PIN;
+        for (int i = 0; i < 1000000; i++) {}
+    }
+}
